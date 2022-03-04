b9e181482e479f40fec25bb07fed4cf1ea1f95e8
Mathias <math8802@stud.kea.dk>
2022-03-04 01:52:58 +0100

    redid directory

---
+++ Svelte-SelfStudy/Part1/svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
