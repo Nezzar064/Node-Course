1b4a5064e6d15d16caaf4f37fecb2273f50a6296
Nezzar064 <72224094+Nezzar064@users.noreply.github.com>
2022-03-04 01:51:04 +0100

    Delete 4. Svelte-Selfstudy directory

--- 4. Svelte-Selfstudy/src/lib/SecondNested.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    export let number = 'WHO KNOOOOOWS';
-</script>
-
-<p>My favorite number is: {number}</p>
--- 4. Svelte-Selfstudy/src/lib/ThirdNested.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let name;
-    export let age;
-    export let city;
-</script>
-
-<p>Hello I am <strong>{name}</strong>, I am <strong>{age}</strong> years old and live in <strong>{city}</strong></p>
