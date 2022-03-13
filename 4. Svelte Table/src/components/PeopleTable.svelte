2d4f21c9c336976702afcd1f65fa675cbe6f2c11
Mathias <math8802@stud.kea.dk>
2022-03-13 23:56:10 +0100

    table exercise

---
+++ 4. Svelte Table/src/components/PeopleTable.svelte
@@ -0,0 +1,35 @@
+<script>
+    import TableHead from "./TableHead.svelte";
+    import TableRow from "./TableRow.svelte";
+    import { faker } from "@faker-js/faker";
+
+    function generateData(count) {
+        let tableData = [];
+
+        while (count != 0) {
+            const person = {
+                id: faker.mersenne.rand(1, 1000),
+                name: faker.name.findName(),
+                branch: faker.git.branch(),
+                commit: faker.git.shortSha(),
+                message: faker.git.commitMessage(),
+                avatar: faker.image.avatar(),
+            };
+
+            tableData.push(person);
+
+            count--;
+        }
+
+        return tableData;
+    }
+
+    let tableData = generateData(20);
+</script>
+
+<table class="table is-narrow">
+    <TableHead tableHeader={tableData} />
+    <TableRow data={tableData} />
+</table>
+
+
---
+++ 4. Svelte Table/src/components/TableHead.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let tableHeader;
+</script>
+
+
+<thead>
+    <tr>
+        {#each Object.keys(tableHeader[0]) as header}
+            <th>
+                {header}
+            </th>
+        {/each}
+    </tr>
+</thead>
